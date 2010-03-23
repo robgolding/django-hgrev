433b36c0db27d697d91d454919235f00f3969721
Rob Golding <rob@robgolding.com>
2010-03-23 17:36:41 +0000

    Added a try..except so that exceptions don't mess up the tag

--- hgrev/templatetags/__init__.py
+++ hgrev/templatetags/__init__.py
@@ -1,9 +1,12 @@
 def get_revision():
-	from hgrev import settings, ui, hg
-
-	repo = hg.repository(ui.ui(), settings.HGREV_HG_PATH)
-	fctx = repo.filectx(None, 'tip')
-
-	return 'r%d:%s' % (fctx.rev(), fctx.hex()[:4])
+	try:
+		from hgrev import settings, ui, hg
+		
+		repo = hg.repository(ui.ui(), settings.HGREV_HG_PATH)
+		fctx = repo.filectx(None, 'tip')
+		
+		return 'r%d:%s' % (fctx.rev(), fctx.hex()[:4])
+	except Exception:
+		return ''
 
 REVISION = get_revision()
